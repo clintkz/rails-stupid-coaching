18d29a7caef5ac57f5129421d15f6c2b8d4d1aaa
Clint Ziervogel <photon01.joins@icloud.com>
2023-10-19 20:33:27 +0200

    correctly working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,10 +8,13 @@ class QuestionsController < ApplicationController
   def answer
     @ask = params[:ask]
 
-    if @ask == STOP_MESSAGE || @ask == STOP_MESSAGE.upcase
-      @answer = "Great!"
+    # if @ask == STOP_MESSAGE || @ask == STOP_MESSAGE.upcase
+    if @ask == "What is the time?"
+      @answer = Time.now
     elsif @ask.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
+    elsif @ask == "Hello"
+      @answer = "Whell hello there"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
